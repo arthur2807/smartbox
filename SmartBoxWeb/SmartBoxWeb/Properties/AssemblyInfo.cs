0ff06d83ddb651f6787c90377370f754ea14bacb
MarceloLeandro <marcelo_leandro111@hotmail.com>
2014-08-20 19:13:58 -0300

    Marcelo Commit

---
+++ SmartBoxWeb/SmartBoxWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do 
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("SmartBoxWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmartBoxWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// A configuração de ComVisible como false torna os tipos deste assembly invisíveis 
+// para componentes COM. Se for necessário acessar um tipo neste assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("f698524f-eadd-40a1-8566-72e33d6e1f45")]
+
+// As informações de versão de um assembly consistem nos seguintes quatro valores:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// Você pode especificar todos os valores ou pode usar como padrão os Números da Versão e de Revisão 
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
